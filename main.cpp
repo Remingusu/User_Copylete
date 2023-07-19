28d92d3640f3b71491850da3ed809c1dc3fdc34e
LeweenH <lewee.m33@gmail.com>
2023-07-19 22:54:41 +0200

    list all users, remove uninstersting folders, add to vecotr

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <filesystem>
+#include <vector>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    std::vector<std::string> folder_list;
+    for (const auto& foldername :  std::filesystem::directory_iterator("C:\\Users"))
+    {
+        if (std::filesystem::is_directory(foldername))
+        {
+            folder_list.push_back(foldername.path().generic_string());
+        }
+    }
+    std::remove(folder_list.begin(), folder_list.end(), "C:/Users/Public");
+    std::remove(folder_list.begin(), folder_list.end(), "C:/Users/Default");
+    std::remove(folder_list.begin(), folder_list.end(), "C:/Users/Default User");
+    std::remove(folder_list.begin(), folder_list.end(), "C:/Users/All Users");
+
+    for (const auto& foldername : folder_list)
+    {
+        std::cout << foldername << std::endl;
+    }
+
     return 0;
 }
